e86d8936c1e0338b9729da5daac9a70a21aa5c86
stephen.whipp <stephenwhippuk@gmail.com>
2025-01-25 00:26:48 +0000

    basic boiler plate completed

---
+++ c/file/1-reading-text/main.c
@@ -0,0 +1,8 @@
+#include <stdincludes.h>
+
+int main(){
+    printf("Reading File example... \n");
+
+    
+    return EXIT_SUCCESS;
+}
---
+++ c/file/include/stdincludes.h
@@ -0,0 +1,7 @@
+#ifndef _STD_INCLUDES
+#define _STD_INCLUDES
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
