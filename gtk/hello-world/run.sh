d70a7d2aecdcd931e9997c85c07f0e47376cbe93
root <root@DESKTOP-56O0BLC.>
2025-01-25 00:07:19 +0000

    fixed the settings to allow pkg-config to build gtk4

--- gtk/hello-world/run.sh
+++ gtk/hello-world/run.sh
@@ -1,1 +1,2 @@
-gcc `pkg-config --cflags glib-2.0 --libs glib-2.0` main.c -I /root/usr/include/gtk-4.0 -I /usr/include/cairo -I /root/usr/include/pango-1.0 -I /root/usr/include/harfbuzz -I /usr/include/gdk-pixbuf-2.0 -I /usr/include/graphene-1.0 -I /usr/lib/x86_64-linux-gnu/graphene-1.0/include
+gcc main.c `pkg-config --cflags gtk4` `pkg-config --libs gtk4` -o hello
+./hello
