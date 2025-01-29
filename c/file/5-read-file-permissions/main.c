1b4136ce70a50f898a68be64f8abbdb84eb1365c
stephen.whipp <stephenwhippuk@gmail.com>
2025-01-29 00:06:56 +0000

    added checks for buffer size and refactored and added to main to handle this error

--- c/file/5-read-file-permissions/main.c
+++ c/file/5-read-file-permissions/main.c
@@ -7,14 +7,21 @@ int main(){
 
     char buffer[BUFFER_LENGTH];
 
-    int result = getPermissions("../data/data.txt", buffer);
+    int result = getPermissions("../data/data.txt", buffer, BUFFER_LENGTH);
 
     switch(result){
         case ERROR_READING_FILE:
             printf("Unable to read file");
-            exit(EXIT_FAILURE);
+            break;
+        case BUFFER_TO_SMALL:
+            printf("insufficient space in buffer to hold permissions");
+            break;
+        default:
+            printf("target\t%s\n", buffer);
     }
-    printf("target\t%s\n", buffer);
-
+  
+    if(result){
+        exit(EXIT_FAILURE);
+    }
     exit(EXIT_SUCCESS);
 }
--- c/file/5-read-file-permissions/permission.h
+++ c/file/5-read-file-permissions/permission.h
@@ -3,7 +3,8 @@
 
 #define OPERATION_SUCCESS 0
 #define ERROR_READING_FILE 1
+#define BUFFER_TO_SMALL 2
 
-int getPermissions(const char* path, char* output);
+int getPermissions(const char* path, char* buffer, int bufferSize);
 
 #endif
