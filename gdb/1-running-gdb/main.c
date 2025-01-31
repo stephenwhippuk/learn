30413f8d0cff82de9a7c88a437c3d84d95eb8965
stephen.whipp <stephenwhippuk@gmail.com>
2025-01-31 23:12:43 +0000

    simple program to allow breakpoints and watch to be set

---
+++ gdb/1-running-gdb/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// simple program to allow break points and a watch to be set
+int main(){
+    int i = 0;
+    for(i = 0; i < 10; i++)
+        printf("%d\n", i);
+    return i;
+}
